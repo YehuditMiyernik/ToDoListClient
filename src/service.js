8e122e08d3b2af0ba8962f8154df6efb9168fef1
y0533121238@gmail.com <y0533121238@gmail.com>
2025-01-21 15:42:45 +0200

    initial commit

---
+++ src/service.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:5096';
+
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    console.error('Error:', error.response?.status, error.response?.data);
+    return Promise.reject(error);
+  }
+);
+
+export default {
+  getTasks: async () => {
+    const response = await axios.get();
+    return response.data;
+  },
+
+  addTask: async (name) => {
+    var data = { name: name, isComplete: false };
+    await axios.post('', data);
+    return;
+  },
+
+  setCompleted: async (id) => {
+    await axios.put(`/${id}`);
+    return;
+  },
+
+  deleteTask: async (id) => {
+    await axios.delete(`/${id}`);
+    return;
+  }
+};
